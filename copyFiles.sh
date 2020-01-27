8a508338db4268dd1898a22c9d3bac9b19b977fa
georgikoemdzhiev <koemdjiev@gmail.com>
2020-01-27 21:35:40 +0000

    Changes

--- copyFiles.sh
+++ copyFiles.sh
@@ -3,9 +3,16 @@ cp ~/.bashrc ./bashrc
 echo "Copping i3 config"
 cp ~/.i3/config ./i3config
 
+
+if [! -f .git]; then
+	echo "Creating a git repo"
+	git init
+	git remote add origin https://github.com/georgikoemdzhiev/configs.git
+fi
+
 git add *
 
 echo "committing chages..."
 git commit -m "Changes"
-echi "pushing changes upstream..."
+echo "pushing changes upstream..."
 git push 
