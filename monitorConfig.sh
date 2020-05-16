7327be673d5c8fa3fcbe195223b1404fab5e71ed
georgikoemdzhiev <koemdjiev@gmail.com>
2020-05-16 20:14:40 +0100

    Added hdmi option

--- monitorConfig.sh
+++ monitorConfig.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # monitor configurations
-monitorConfigs="Monitor1&2\nMonitor1\nLaptop Monitor"
+monitorConfigs="Monitor1&2\nMonitor1\nLaptop Monitor\nHDMI"
 chosen=$(echo -e "$monitorConfigs" | dmenu -i -p "Select Monitor configuration: ")
 
 case "$chosen" in 
     "Monitor1&2") xrandr --output eDP-1 --off --output DP-1-1 --primary --auto --left-of DP-1-3 --output DP-1-3 --auto;;
     "Monitor1") xrandr --output eDP-1 --off --output DP-1-1 --auto --output DP-1-3 --off;;
     "Laptop Monitor") xrandr --output eDP-1 --auto --output DP-1-1 --off --output DP-1-3 --off;;
+    "HDMI") xrandr --output HDMI-1 --mode 1920x1080 --output eDP-1 --off;;
 esac
