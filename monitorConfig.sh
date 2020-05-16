8c109b20c208378413334450b76b108c943ced2c
georgikoemdzhiev <koemdjiev@gmail.com>
2020-05-16 20:03:57 +0100

    fixed 2 monitor output

--- monitorConfig.sh
+++ monitorConfig.sh
@@ -5,7 +5,7 @@ monitorConfigs="Monitor1&2\nMonitor1\nLaptop Monitor"
 chosen=$(echo -e "$monitorConfigs" | dmenu -i -p "Select Monitor configuration: ")
 
 case "$chosen" in 
-    "Monitor1&2") xrandr --output eDP-1 --off --output DP-1-3 --auto --right-of DP-1-1 --output DP-1-1 --primary;;
+    "Monitor1&2") xrandr --output eDP-1 --off --output DP-1-1 --primary --auto --left-of DP-1-3 --output DP-1-3 --auto;;
     "Monitor1") xrandr --output eDP-1 --off --output DP-1-1 --auto --output DP-1-3 --off;;
     "Laptop Monitor") xrandr --output eDP-1 --auto --output DP-1-1 --off --output DP-1-3 --off;;
 esac
