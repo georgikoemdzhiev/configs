45ecf4fb8fb4bc6184bc1d6dc76766b4aefaa2a7
georgikoemdzhiev <koemdjiev@gmail.com>
2021-01-04 12:07:11 +0000

    show progress when syncing files with rsync

--- backupNextCloud.sh
+++ backupNextCloud.sh
@@ -3,4 +3,4 @@
 # Script that backups local NextCloud running in local area network server with address
 # 192.168.1.141
 # The destination of the back is a a HHD drive connected to the machine that executes the script
-sudo rsync -aAX --delete --exclude "lost+found" pi@192.168.1.141:/media/pi/SSD/ /run/media/georgi/HHD/NextCloudBackup
+sudo rsync -aAXP --delete --exclude "lost+found" pi@192.168.1.141:/media/pi/SSD/ /run/media/georgi/HHD/NextCloudBackup
