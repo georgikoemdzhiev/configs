3b65b41860b233fdefbe44d66ced9de705c0340d
georgikoemdzhiev <koemdjiev@gmail.com>
2021-01-09 10:44:59 +0000

    replace IP address of NextCloud server with alias

--- backupNextCloud.sh
+++ backupNextCloud.sh
@@ -5,7 +5,7 @@
 
 if mount | grep /run/media/georgi/HHD > /dev/null; then
   # The destination of the back is a a HHD drive connected to the machine that executes the script
-  sudo rsync -aAX -P --delete --exclude "lost+found" pi@192.168.1.207:/home/pi/NextCloud/SSD/ /run/media/georgi/HHD/NextCloudBackup
+  sudo rsync -aAX -P --delete --exclude "lost+found" pi@TinyPiServer:/home/pi/NextCloud/SSD/ /run/media/georgi/HHD/NextCloudBackup
 else
   echo "HHD device not Mounted. Mount it at '/run/media/georgi/HHD' first and rerun the script"
 fi
